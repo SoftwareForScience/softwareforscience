8489fb4e19031e80a5b10b98377a4bceffd4490c
Heiko van der Heijden <h.j.m.van.der.heijden@hva.nl>
2018-06-22 11:02:25 +0200

    cleanup the sass file

--- assets/js/software4science.js
+++ assets/js/software4science.js
@@ -2,11 +2,11 @@
     "use strict"
     $(document).ready(function () {
         $('#sidebar-collapse-title').on('click', function () {
-            $('#sidebar').toggleClass('active')
+            $('.sidebar').toggleClass('active')
         })
 
         $('#sidebar-collapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
+            $('.sidebar').toggleClass('active');
         })
     })
 })()
